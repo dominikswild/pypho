9ff8c1359b0005795e03448fd1e0ddb1a135972c
Dominik Wild <dominikswild@gmail.com>
2019-03-13 18:27:13 -0400

    Updated setup script, deleted requirements.txt

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='pyPho',
     version='0.0.1',
     description=('Toolbox for computing optical properties of 2D materials '
                  'embedded in layered, periodic structures'),
     long_description=readme,
     author='Dominik S. Wild',
     author_email='dominikswild@gmail.com',
-    url='https://github.com/dominikswild/module',
+    url='https://github.com/dominikswild/pypho',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
