5c63cbea1c0cf723d752d6a9daa69a9e08fd6e32
dominikswild <47221527+dominikswild@users.noreply.github.com>
2019-03-14 02:00:59 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 from setuptools import setup, find_packages
 
-with open('README.md') as f:
+with open("README.md") as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open("LICENSE") as f:
     license = f.read()
 
 setup(
-    name='pyPho',
-    version='0.0.1',
-    description=('Toolbox for computing optical properties of 2D materials '
-                 'embedded in layered, periodic structures'),
+    name="pyPho",
+    version="0.0.1",
+    description=("A photonics package for Python."),
     long_description=readme,
-    author='Dominik S. Wild',
-    author_email='dominikswild@gmail.com',
-    url='https://github.com/dominikswild/pypho',
+    author="Dominik S. Wild",
+    author_email="dominikswild@gmail.com",
+    url="https://github.com/dominikswild/pypho",
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=("tests", "docs"))
 )
